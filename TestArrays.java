54ce4f8af890addd1bc36d1a7722a9005eb4ca3a
scottszb1987 <scottszb1987@hotmail.com>
2014-12-27 15:32:51 +0800

    creation
    head first java chap 3 quiz

---
+++ TestArrays.java
@@ -0,0 +1,24 @@
+
+public class TestArrays {
+	public static void main(String[] args){
+		int y = 0;
+		int[] index = new int[4];
+		index[0] = 1;
+		index[1] = 3;
+		index[2] = 0;
+		index[3] = 2;
+		String[] islands = new String[4];
+		islands[0] = "Bermuda";
+		islands[1] = "Fiji";
+		islands[2] = "Azeros";
+		islands[3] = "Cozumel";
+		int ref;
+		while(y<4){
+			ref = index[y];
+			System.out.print("island = ");
+			System.out.println(islands[ref]);
+			y++;
+		}
+		
+	}
+}
